c06e59b5cad35adb853f04744d99e31fce6498dd
Luke Spademan <info@lukespademan.com>
2021-12-24 23:13:24 +0000

    initial seperation into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,14 @@
-use std::fmt;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use trust_dns_client::client::{Client, SyncClient};
-use trust_dns_client::op::DnsResponse;
-use trust_dns_client::rr::{DNSClass, Name, RData, Record, RecordType};
-use trust_dns_client::udp::UdpClientConnection;
+use domainfo;
 
-enum QueryType {
-    DomainName(String),
-    IPAddr(String),
-}
-
-struct BGPToolsResponse {
-    asn: u32,
-    ip: String,
-    prefix: String,
-    country: String,
-    registry: String,
-    as_name: String,
-}
-
-impl fmt::Display for BGPToolsResponse {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} ({}) is advertised by {} (AS{} {} {})",
-            self.ip, self.prefix, self.as_name, self.asn, self.registry, self.country,
-        )
-    }
-}
-
-fn dns_lookup(domain: String) {
-    let address = "8.8.8.8:53".parse().unwrap();
-    let conn = UdpClientConnection::new(address).unwrap();
-    let client = SyncClient::new(conn);
-    let name = domain.parse::<Name>().unwrap();
-    let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::A).unwrap();
-
-    let answers: &[Record] = response.answers();
-    let mut prefix = "";
-    for ans in answers.iter() {
-        if let &RData::A(ref ip) = ans.rdata() {
-            println!(
-                "{}{} has IPv4 {}\n{}",
-                prefix,
-                domain,
-                ip,
-                bgp_tools_query(ip.to_string())
-            );
-            prefix = "\n";
-        };
-        if let &RData::AAAA(ref ip) = ans.rdata() {
-            println!(
-                "{}{} has IPv6 {}\n{}",
-                prefix,
-                domain,
-                ip,
-                bgp_tools_query(ip.to_string())
-            );
-            prefix = "\n";
-        };
-    }
-}
-
-fn detect_query_type(query: String) -> QueryType {
-    let dot_count = query.matches(".").count();
-    match dot_count {
-        0 => QueryType::IPAddr(query),
-        3 => {
-            if query
-                .split(".")
-                .filter_map(|x| x.parse::<u32>().ok())
-                .filter(|&x| x < 255)
-                .count()
-                == 4
-            {
-                QueryType::IPAddr(query)
-            } else {
-                QueryType::DomainName(query)
-            }
-        }
-        _ => QueryType::DomainName(query),
-    }
-}
-
-fn bgp_tools_query(query: String) -> BGPToolsResponse {
-    let mut stream = TcpStream::connect("bgp.tools:43").unwrap();
-    let mut buffer = [0; 1024];
-
-    stream.write((query + "\n").as_bytes()).unwrap();
-    stream.read(&mut buffer).unwrap();
-
-    let parts = std::str::from_utf8(&buffer)
-        .unwrap()
-        .split("\n")
-        .collect::<Vec<&str>>()[1]
-        .split("|")
-        .collect::<Vec<&str>>()
-        .iter()
-        .map(|&part| part.trim())
-        .collect::<Vec<&str>>();
-
-    BGPToolsResponse {
-        asn: u32::from_str_radix(parts[0], 10).unwrap(),
-        ip: String::from(parts[1]),
-        prefix: String::from(parts[2]),
-        country: String::from(parts[3]),
-        registry: String::from(parts[4]),
-        as_name: String::from(parts[6]),
-    }
-}
 
 fn main() {
-    let query = detect_query_type(std::env::args().nth(1).expect("No query specified"));
+    let query = domainfo::detect_query_type(std::env::args().nth(1).expect("No query specified"));
     match query {
-        QueryType::IPAddr(q) => {
-            println!("{}", bgp_tools_query(String::from(q)));
+        domainfo::QueryType::IPAddr(q) => {
+            println!("{}", domainfo::bgp_tools_query(String::from(q)));
         }
-        QueryType::DomainName(q) => {
-            dns_lookup(q);
+        domainfo::QueryType::DomainName(q) => {
+            domainfo::dns_lookup(q);
         }
     };
 }
