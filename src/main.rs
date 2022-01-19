9fb247d4a824fa518c961f85edf50f56e8c17d43
Luke Spademan <info@lukespademan.com>
2022-01-19 17:50:19 +0000

    fix panic on no argument

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use domainfo;
 
 fn main() {
-    let query = domainfo::detect_query_type(std::env::args().nth(1).expect("No query specified"));
-    match query {
-        domainfo::QueryType::IPAddr(q) => {
-            print!("{}", domainfo::bgp_tools_query(&q));
+    match std::env::args().nth(1) {
+        Some(query) => {
+            match domainfo::detect_query_type(query) {
+                domainfo::QueryType::IPAddr(q) => {
+                    print!("{}", domainfo::bgp_tools_query(&q));
+                }
+                domainfo::QueryType::DomainName(q) => {
+                    print!("{}", domainfo::dns_lookup(&q));
+                }
+            };
         }
-        domainfo::QueryType::DomainName(q) => {
-            print!("{}", domainfo::dns_lookup(&q));
+        None => {
+            eprintln!(
+                "Please provide a domain name or IP address\nUsage: domainfo <ipaddr or domain>"
+            );
         }
     };
 }
